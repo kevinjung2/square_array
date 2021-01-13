1b919e6bd7b27f45e57810be5ca52fc416e59741
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-13 15:06:29 -0500

    commit tree f9d794d1c69dcd1413cf57dca8a93f30543b798b

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,23 @@
+require "pry"
+# def square_array(array)
+#   # your code here
+#   new_array = Array.new
+#   array.each do |number|
+#     new_array << number ** 2
+#   end
+#   new_array
+# end
+
 def square_array(array)
-  # your code here
+  new_array = Array.new
+  array.each { |number| new_array << number ** 2 }
+  new_array
 end
+
+# def square_array(array)
+#   array.collect { |num| num ** 2 }
+# end
+#
+#
+# squared = square_array([1,2,3,4])
+# puts "#{squared}"
